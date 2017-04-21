b7fb1f3ccd40495990f4e9b3a63a05c368482bee
Raimon Esteve <raimon@nan-tic.com>
2017-04-21 10:08:38 +0200

    Upgrade last changes from 4.0 branch

--- sale.py
+++ sale.py
@@ -1,202 +1,77 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Bool, Eval, Equal, Not, Or
+from trytond.pyson import Eval, Bool
+from datetime import date
+
+__all__ = ['Sale', 'SaleLine']
 
 
-__all__ = ['Sale', 'SaleLine', 'Work']
-
-
-class Work:
+class SaleLine:
+    __name__ = 'sale.line'
     __metaclass__ = PoolMeta
-    __name__ = 'project.work'
+    project = fields.Many2One('project.work', 'Project', readonly=True,
+        select=True)
 
-    sale_lines = fields.One2Many('sale.line', 'task', 'Sale Lines')
+    @classmethod
+    def copy(cls, lines, default=None):
+        if default is None:
+            default = {}
+        default['project'] = None
+        return super(SaleLine, cls).copy(lines, default=default)
 
 
 class Sale:
-    __metaclass__ = PoolMeta
     __name__ = 'sale.sale'
-    work = fields.Many2One('project.work', 'Project',
+    __metaclass__ = PoolMeta
+    parent_project = fields.Many2One('project.work', 'Parent Project',
+        select=True,
+        domain=[('party', '=', Eval('party'))],
         states={
-            'readonly': Or(Not(Equal(Eval('invoice_method'), 'manual')),
-                Not(Equal(Eval('shipment_method'), 'manual'))),
+            'readonly': Eval('state') != 'draft',
             },
-        domain=[
-            ('type', '=', 'project'),
-            ('company', '=', Eval('company', -1)),
-            ('party', '=', Eval('party', -1)),
-            ],
-        depends=['company', 'party', 'invoice_method', 'shipment_method'])
-    create_project = fields.Boolean('Create Project',
-        states={
-            'readonly': Or(Or(Not(Equal(Eval('invoice_method'), 'manual')),
-                Not(Equal(Eval('shipment_method'), 'manual'))),
-                Bool(Eval('work'))),
-            },
-        depends=['work', 'invoice_method', 'shipment_method'])
-
-    @classmethod
-    def __setup__(cls):
-        super(Sale, cls).__setup__()
-        cls._error_messages.update({
-                'check_project_error': ('Invalid combination of shipment and '
-                    'invoicing methods on sale "%s" for process Projects.\n'
-                    'You must set both to "manual"'),
-                'check_sale_back_error': 'You can not set the state of the '
-                    'sale "%s" to Draft when it has not been processed with '
-                    'both methods (shipment & invoicing) set to "manual".'
-                })
-        cls._transitions |= set((
-                ('done', 'draft'),
-                ))
-        cls._buttons.update({
-                'load_project': {
-                    'invisible': ~Eval('state').in_(['draft', 'quotation']),
-                    },
-                })
-
-    @fields.depends('create_project', 'work', 'invoice_method',
-        'shipment_method')
-    def on_change_with_create_project(self):
-        if (self.work or self.invoice_method != 'manual' or
-                self.invoice_method != 'manual'):
-            return False
-        return self.create_project
-
-    @classmethod
-    def check_project(cls, sales):
-        for sale in sales:
-            if sale.work or sale.create_project:
-                if (sale.invoice_method != 'manual' or
-                        sale.shipment_method != 'manual'):
-                    cls.raise_user_error('check_project_error',
-                        (sale.rec_name,))
-
-    @classmethod
-    def update_project(cls, sales):
-        Line = Pool().get('sale.line')
-        for sale in sales:
-            if sale.work:
-                Line.update_tasks(sale.lines)
-
-    @classmethod
-    def quote(cls, sales):
-        super(Sale, cls).quote(sales)
-        cls.check_project(sales)
-
-    @classmethod
-    def confirm(cls, sales):
-        super(Sale, cls).confirm(sales)
-        cls.check_project(sales)
+        depends=['state', 'party'])
 
     @classmethod
     def process(cls, sales):
         super(Sale, cls).process(sales)
         cls.create_projects(sales)
 
-    def _get_project(self):
-        project = self.work
-        if not project:
-            Work = Pool().get('project.work')
-            project = Work()
-            project.name = self.rec_name
-            project.type = 'project'
-            project.company = self.company
-            project.project_invoice_method = 'progress'
-            project.invoice_product_type = 'service'
-            project.party = self.party
-            project.party_address = self.invoice_address
-            project.progress_quantity = 0.0
-            project.quantity = 0.0
-        project.readonly = True
-        return project
-
     @classmethod
     def create_projects(cls, sales):
+        pool = Pool()
+        SaleLine = pool.get('sale.line')
         for sale in sales:
-            if sale.work is None and not sale.create_project:
+            if not sale.parent_project:
                 continue
             project = sale._get_project()
-            sale.create_project_from_sales(project)
-            sale.work = project
-        cls.save(sales)
+            project.save()
+            SaleLine.write([x for x in sale.lines if x.type == 'line'],
+                {'project': project.id})
 
-    @fields.depends('task_parent')
-    def create_project_from_sales(self, parent_project, parent_line=None):
-        lines = [x for x in self.lines if parent_line == x.parent]
-        for line in lines:
-            if line.type == 'line' and line.quantity == 0.00:
-                continue
-            if line.task:
-                task = line.task
-            else:
-                task = line._get_task()
-                task.sale_lines = []
-                task.sale_lines += (line,)
-                if parent_project:
-                    task.parent = parent_project
-
-            task.quantity = task.sale_line_quantities
-            task.save()
-            if line.childs:
-                self.create_project_from_sales(task, line)
-
-    @classmethod
-    def load_project(cls, sales):
-        for sale in sales:
-            if not sale.work:
-                return
-            sale.create_lines_from_project(sale.work.children)
-        cls.save(sales)
-
-    def create_lines_from_project(self, project, parent_line=None):
-        for task in project:
-            sale_line = task.get_sale_line(parent_line)
-            if parent_line:
-                sale_line.parent = parent_line
-            sale_line.save()
-            self.lines += (sale_line,)
-            if task.children:
-                self.create_lines_from_project(task.children, sale_line)
-
-
-class SaleLine:
-    __metaclass__ = PoolMeta
-    __name__ = 'sale.line'
-    task = fields.Many2One('project.work', 'Task')
-
-    @classmethod
-    def __setup__(cls):
-        super(SaleLine, cls).__setup__()
-        if hasattr(SaleLine, '_allow_modify_after_draft'):
-            cls._allow_modify_after_draft |= set(['task'])
-
-    def _get_task(self):
-        task = self.task
-        if task:
-            return task
-
+    def _get_project(self):
         Work = Pool().get('project.work')
-        task = Work()
-        task.quantity = self.quantity
-        task.type = 'task'
-        if self.type == 'title':
-            task.type = 'project'
-            task.quantity = 0.0
 
-        task.name = self.rec_name
-        task.company = self.sale.company
-        task.project_invoice_method = 'progress'
-        task.progress_quantity = 0.0
-        if self.type == 'title' or self.product and \
-                self.product.type == 'service':
-            task.invoice_product_type = 'service'
-        else:
-            task.invoice_product_type = 'goods'
-            task.product_goods = self.product
-            task.on_change_product_goods()
-            task.list_price = self.unit_price
+        project = None
+        line = self.lines and self.lines[0]
+        if line.project:
+            return project
 
-        return task
+        project = Work()
+        project.name = self.rec_name
+        project.type = 'project'
+        project.product_goods = self.parent_project.product_goods
+        project.uom = self.parent_project.uom
+        project.company = self.company
+        project.project_invoice_method = \
+            self.parent_project.project_invoice_method
+        project.invoice_product_type = self.parent_project.invoice_product_type
+        project.parent = self.parent_project
+        project.party = self.party
+        project.party_address = self.invoice_address
+        project.progress_quantity = 0
+        project.quantity = 1
+        project.start_date = date.today()
+        project.list_price = self.untaxed_amount
+        return project
--- work.py
+++ work.py
@@ -1,9 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import fields
-from trytond.pool import PoolMeta, Pool
-from trytond.pyson import Bool, Eval, If
 
+from trytond.model import fields
+from trytond.pool import PoolMeta
 
 __all__ = ['Work']
 
@@ -14,47 +13,11 @@ class Work:
     sale_lines = fields.One2Many('sale.line', 'task', 'Sale Lines',
         readonly=True)
 
-    @property
-    def sale_line_quantities(self):
-        return sum([l.quantity for l in self.sale_lines \
-            if l.type == 'line'], 0.0)
-
     @classmethod
-    def __setup__(cls):
-        super(Work, cls).__setup__()
-        cls.quantity.states['readonly'] = Bool(Eval('sale_lines'))
-        if 'sale_lines' not in cls.quantity.depends:
-            cls.quantity.depends.append('sale_lines')
-
-    def get_sale_line(self, parent):
-        pool = Pool()
-        ModelData = pool.get('ir.model.data')
-        Uom = pool.get('product.uom')
-        SaleLine = pool.get('sale.line')
-
-        sale_line = SaleLine()
-        type_ = 'line'
-        if self.type == 'project':
-            type_ = 'title'
-
-        sale_line.type = type_
-        sale_line.description = self.name
-        sale_line.task = self
-        sale_line.childs = []
-
-        if type_ == 'title':
-            return sale_line
-
-        sale_line.quantity = 0
-        if self.invoice_product_type == 'goods':
-            sale_line.product = self.product_goods
-            sale_line.on_change_product()
-            sale_line.unit = self.uom
-            sale_line.unit_price = self.list_price
-        if self.invoice_product_type == 'service':
-            sale_line.product = self.product
-            sale_line.on_change_product()
-            sale_line.unit = Uom(ModelData.get_id('product', 'uom_hour'))
-            sale_line.unit_price = self.list_price
-
-        return sale_line
+    def copy(cls, works, default=None):
+        if default is None:
+            default = {}
+        else:
+            default = default.copy()
+        default['sale_lines'] = None
+        return super(Work, cls).copy(works, default=default)
